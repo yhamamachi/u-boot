825d5de47e96e442b9caa90fff6d591a441f0fb1
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-06-04 10:04:56 +0300

    xen: Port Xen grant table driver from mini-os
    Make required updates to run on u-boot.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Signed-off-by: Anastasiia Lukianenko <anastasiia_lukianenko@epam.com>

--- drivers/xen/hypervisor.c
+++ drivers/xen/hypervisor.c
@@ -38,6 +38,7 @@
 
 #include <xen/hvm.h>
 #include <xen/events.h>
+#include <xen/gnttab.h>
 #include <xen/xenbus.h>
 #include <xen/interface/memory.h>
 
@@ -275,5 +276,6 @@ void xen_init(void)
 	map_shared_info(NULL);
 	init_events();
 	init_xenbus();
+	init_gnttab();
 }
 
